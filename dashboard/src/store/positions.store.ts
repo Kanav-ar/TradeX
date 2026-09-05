42e658653428441020e712487eba325ed1163d93
Kanav-ar <kanava879@gmail.com>
2026-09-06 02:54:32 +0530

    feat: add refreshPositions function to fetch and update positions in the store

--- dashboard/src/store/positions.store.ts
+++ dashboard/src/store/positions.store.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import type { Position } from "../types/position.types";
+import { getPositions } from "../api/position.api";
 
 interface PositionStore {
   allPositions: Position[];
   setAllPositions: (positions: Position[]) => void;
+  refreshPositions: () => Promise<void>;
 }
 
 export const usePositionStore = create<PositionStore>((set) => ({
@@ -13,5 +15,10 @@ export const usePositionStore = create<PositionStore>((set) => ({
     set({
       allPositions: positions,
     });
+  },
+
+  refreshPositions: async () => {
+    const positions = await getPositions();
+    set({ allPositions: positions });
   },
 }));
