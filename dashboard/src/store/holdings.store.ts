dea487554d209b29c021c89c3abbe88e0dd110e0
Kanav-ar <kanava879@gmail.com>
2026-09-06 02:52:43 +0530

    feat: add refreshHoldings function to fetch and update all holdings in the store

--- dashboard/src/store/holdings.store.ts
+++ dashboard/src/store/holdings.store.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
 import type { Holding } from "../types/holding.types";
+import { getHoldings } from "../api/holding.api";
 
 interface HoldingStore {
   allHoldings: Holding[];
   setAllHoldings: (holdings: Holding[]) => void;
+  refreshHoldings: () => Promise<void>;
 }
 
 export const useHoldingStore = create<HoldingStore>((set) => ({
   allHoldings: [],
 
   setAllHoldings: (holdings) => {
+    set({ allHoldings: holdings });
+  },
+  refreshHoldings: async () => {
+    const holdings = await getHoldings();
     set({ allHoldings: holdings });
   },
 }));
