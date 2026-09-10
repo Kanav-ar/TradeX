b9ab715acc43fb8a2250f5b929fd4bc3b8ee0d67
Kanav-ar <kanava879@gmail.com>
2026-09-10 21:38:08 +0530

    feat: update API base URLs to use environment variables and clean up configuration files

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api/v1/users",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/users`,
   headers: {
     "Content-Type": "application/json",
   },
